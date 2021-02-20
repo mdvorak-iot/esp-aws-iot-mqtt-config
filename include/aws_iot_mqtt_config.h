48f94883464f2bc0afaf6ab97c3a9853790fae87
Michal Dvorak <michal@mdvorak.org>
2021-02-20 23:29:20 +0100

    shorter nvs constants
    original were too long for nvs implementation, limit is 15 chars

--- include/aws_iot_mqtt_config.h
+++ include/aws_iot_mqtt_config.h
@@ -25,14 +25,14 @@ esp_err_t aws_iot_mqtt_config_store(const esp_mqtt_client_config_t *mqtt_cfg);
  */
 void aws_iot_mqtt_config_free_unused(esp_mqtt_client_config_t *mqtt_cfg);
 
-#define AWS_IOT_MQTT_CONFIG_NVS_NAME "aws_iot_mqtt_config"
+#define AWS_IOT_MQTT_CONFIG_NVS_NAME "aws_iot_mqtt"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_HOST "host"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_PORT "port"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_URI "uri"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_ID "client_id"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_CERT "client_cert"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY "client_key"
-#define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENTKEY_PASSWORD "clientkey_password"
+#define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENTKEY_PASSWORD "clientkey_pass"
 
 #ifdef __cplusplus
 }
