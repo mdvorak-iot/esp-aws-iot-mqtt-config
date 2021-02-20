95dfc00a5e60a7dcc465d6b40a2d661ff7e22efa
Michal Dvorak <michal@mdvorak.org>
2021-02-20 23:26:55 +0100

    store and free methods

--- include/aws_iot_mqtt_config.h
+++ include/aws_iot_mqtt_config.h
@@ -8,7 +8,22 @@
 extern "C" {
 #endif
 
+esp_err_t aws_iot_mqtt_config_load(esp_mqtt_client_config_t *mqtt_cfg);
+
 esp_err_t aws_iot_mqtt_config_store(const esp_mqtt_client_config_t *mqtt_cfg);
+
+/**
+ * @brief Frees some memory allocated by the aws_iot_mqtt_config_load() function.
+ *
+ * @details Call this after esp_mqtt_client_init() or esp_mqtt_set_config() functions.
+ *
+ * @details Frees memory from: host, uri, client_id and clientkey_password attributes.
+ * Certificates cannot be freed, since mqtt does not internally make a copy -
+ * they must remain allocated during whole lifetime of a mqtt client.
+ *
+ * @param mqtt_cfg MQTT configuration object.
+ */
+void aws_iot_mqtt_config_free_unused(esp_mqtt_client_config_t *mqtt_cfg);
 
 #define AWS_IOT_MQTT_CONFIG_NVS_NAME "aws_iot_mqtt_config"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_HOST "host"
@@ -17,7 +32,7 @@ esp_err_t aws_iot_mqtt_config_store(const esp_mqtt_client_config_t *mqtt_cfg);
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_ID "client_id"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_CERT "client_cert"
 #define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY "client_key"
-#define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY_PASSWORD "clientkey_password"
+#define AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENTKEY_PASSWORD "clientkey_password"
 
 #ifdef __cplusplus
 }
--- src/aws_iot_mqtt_config.c
+++ src/aws_iot_mqtt_config.c
@@ -1,9 +1,6 @@
 #include "aws_iot_mqtt_config.h"
-#include <esp_log.h>
 #include <mqtt_client.h>
 #include <nvs.h>
-
-static const char TAG[] = "aws_iot_mqtt_config";
 
 #define HANDLE_ERROR(expr, action)  \
     {                               \
@@ -21,8 +18,8 @@ static esp_err_t nvs_helper_set_blob_or_string(nvs_handle_t handle, const char *
 
     if (length == 0)
     {
-        // Must be string
-        length = strlen(value);
+        // Must be string, include null terminator
+        length = strlen(value) + 1;
     }
 
     return nvs_set_blob(handle, key, value, length);
@@ -37,22 +34,95 @@ static esp_err_t nvs_helper_set_string(nvs_handle_t handle, const char *key, con
     return nvs_set_str(handle, key, value);
 }
 
-esp_err_t aws_iot_mqtt_config_store(const esp_mqtt_client_config_t *mqtt_cfg)
+static esp_err_t nvs_helper_get_string(nvs_handle_t handle, const char *key, const char **str, size_t *str_len)
 {
-    esp_err_t err = ESP_FAIL;
-    nvs_handle_t handle = 0;
+    // Already contains something, this is probably an user error
+    if (*str != NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Get value length, including null terminating character
+    size_t value_len = 0;
+    nvs_get_str(handle, key, NULL, &value_len); // ignore error
+
+    if (value_len)
+    {
+        char *value = malloc(value_len);
+        esp_err_t err = nvs_get_str(handle, key, value, &value_len);
+        if (err != ESP_OK)
+        {
+            free(value);
+            return err;
+        }
+
+        *str = value;
+        if (str_len) *str_len = value_len - 1; // minus null terminating character
+    }
+
+    return ESP_OK;
+}
+
+static esp_err_t nvs_helper_get_blob(nvs_handle_t handle, const char *key, const char **blob, size_t *blob_len)
+{
+    // Already contains something, this is probably an user error
+    if (*blob != NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Get value length, including null terminating character
+    size_t value_len = 0;
+    nvs_get_blob(handle, key, NULL, &value_len); // ignore error
+
+    if (value_len)
+    {
+        char *value = malloc(value_len);
+        esp_err_t err = nvs_get_blob(handle, key, value, &value_len); // this should not fail, if we already got length
+        if (err != ESP_OK)
+        {
+            free(value);
+            return err;
+        }
+
+        *blob = value;
+        if (blob_len) *blob_len = value_len;
+    }
+    else
+    {
+        *blob = NULL;
+        if (blob_len) *blob_len = 0;
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t aws_iot_mqtt_config_load(esp_mqtt_client_config_t *mqtt_cfg)
+{
+    if (mqtt_cfg == NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
 
     // Open
-    HANDLE_ERROR(err = nvs_open(AWS_IOT_MQTT_CONFIG_NVS_NAME, NVS_READWRITE, &handle), goto error);
+    nvs_handle_t handle = 0;
+    esp_err_t err = nvs_open(AWS_IOT_MQTT_CONFIG_NVS_NAME, NVS_READONLY, &handle);
+    if (err != ESP_OK)
+    {
+        return err;
+    }
 
-    // Store
-    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_HOST, mqtt_cfg->host), goto error);
-    HANDLE_ERROR(err = nvs_set_u32(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_PORT, mqtt_cfg->port), goto error);
-    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_URI, mqtt_cfg->uri), goto error);
-    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_ID, mqtt_cfg->client_id), goto error);
-    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_CERT, mqtt_cfg->client_cert_pem, mqtt_cfg->client_cert_len), goto error);
-    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY, mqtt_cfg->client_key_pem, mqtt_cfg->client_key_len), goto error);
-    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY_PASSWORD, mqtt_cfg->clientkey_password, mqtt_cfg->clientkey_password_len), goto error);
+    // Load
+    HANDLE_ERROR(err = nvs_helper_get_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_HOST, &mqtt_cfg->host, NULL), goto error);
+    mqtt_cfg->port = 0; // reset and ignore error
+    nvs_get_u32(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_PORT, &mqtt_cfg->port);
+    HANDLE_ERROR(err = nvs_helper_get_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_URI, &mqtt_cfg->uri, NULL), goto error);
+    HANDLE_ERROR(err = nvs_helper_get_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_ID, &mqtt_cfg->client_id, NULL), goto error);
+    HANDLE_ERROR(err = nvs_helper_get_blob(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_CERT, &mqtt_cfg->client_cert_pem, &mqtt_cfg->client_cert_len), goto error);
+    HANDLE_ERROR(err = nvs_helper_get_blob(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY, &mqtt_cfg->client_key_pem, &mqtt_cfg->client_key_len), goto error);
+    size_t clientkey_password_len = 0; // value length in struct is an int, not size_t, for some reason
+    HANDLE_ERROR(err = nvs_helper_get_blob(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENTKEY_PASSWORD, &mqtt_cfg->clientkey_password, &clientkey_password_len), goto error);
+    mqtt_cfg->clientkey_password_len = clientkey_password_len;
 
     // Commit
     err = nvs_commit(handle);
@@ -61,4 +131,58 @@ error:
     // Close and exit
     nvs_close(handle);
     return err;
+}
+
+esp_err_t aws_iot_mqtt_config_store(const esp_mqtt_client_config_t *mqtt_cfg)
+{
+    if (mqtt_cfg == NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Open
+    nvs_handle_t handle = 0;
+    esp_err_t err = nvs_open(AWS_IOT_MQTT_CONFIG_NVS_NAME, NVS_READWRITE, &handle);
+    if (err != ESP_OK)
+    {
+        return err;
+    }
+
+    // Store
+    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_HOST, mqtt_cfg->host), goto error);
+    HANDLE_ERROR(err = nvs_set_u32(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_PORT, mqtt_cfg->port), goto error);
+    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_URI, mqtt_cfg->uri), goto error);
+    HANDLE_ERROR(err = nvs_helper_set_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_ID, mqtt_cfg->client_id), goto error);
+    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_CERT, mqtt_cfg->client_cert_pem, mqtt_cfg->client_cert_len), goto error);
+    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENT_KEY, mqtt_cfg->client_key_pem, mqtt_cfg->client_key_len), goto error);
+    HANDLE_ERROR(err = nvs_helper_set_blob_or_string(handle, AWS_IOT_MQTT_CONFIG_NVS_KEY_CLIENTKEY_PASSWORD, mqtt_cfg->clientkey_password, mqtt_cfg->clientkey_password_len), goto error);
+
+    // Commit
+    err = nvs_commit(handle);
+
+error:
+    // Close and exit
+    nvs_close(handle);
+    return err;
+}
+
+void aws_iot_mqtt_config_free_unused(esp_mqtt_client_config_t *mqtt_cfg)
+{
+    if (mqtt_cfg == NULL)
+    {
+        return;
+    }
+
+    free((char *)mqtt_cfg->host);
+    mqtt_cfg->host = NULL;
+
+    free((char *)mqtt_cfg->uri);
+    mqtt_cfg->uri = NULL;
+
+    free((char *)mqtt_cfg->client_id);
+    mqtt_cfg->client_id = NULL;
+
+    free((char *)mqtt_cfg->clientkey_password);
+    mqtt_cfg->clientkey_password = NULL;
+    mqtt_cfg->clientkey_password_len = 0;
 }
